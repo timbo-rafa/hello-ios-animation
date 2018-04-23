90cba4c859c89adcc81423225eff6f586d726e1f
Student <student@setass-mac-mini-20.local>
2018-04-23 18:54:21 -0400

    Added 3 ViewControllers

---
+++ MAPD724-W2018-Lesson13/SecondViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    
+}
---
+++ MAPD724-W2018-Lesson13/StartViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+
+}
